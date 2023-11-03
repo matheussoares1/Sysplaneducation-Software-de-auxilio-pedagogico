ee8a2f1e4bfed019aa13d9b8ed2a2b9c4c5b9f52
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2023-11-03 09:22:09 -0300

    Update CREATE_TABLES.sql

--- BANCO DE DADOS/CREATE_TABLES.sql
+++ BANCO DE DADOS/CREATE_TABLES.sql
@@ -226,3 +226,181 @@ CREATE TABLE planejamento_sociologia_t(
     FOREIGN KEY (habilidades) REFERENCES humanashab (habilidade) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (competencias_ger) REFERENCES humanascg (competencias_gerais) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (competencias_esp) REFERENCES humanasce (competencias_especificas) ON UPDATE CASCADE ON DELETE CASCADE);
+
+
+CREATE TABLE naturezacg (
+    id_comp_gerais INT PRIMARY KEY AUTO_INCREMENT,
+    competencia_geral VARCHAR(400) NOT NULL UNIQUE
+);
+
+CREATE TABLE naturezace (
+    id_comp_esp INT PRIMARY KEY AUTO_INCREMENT,
+    competencia_especifica VARCHAR(400) NOT NULL UNIQUE
+);
+
+CREATE TABLE naturezahab (
+    id_habilidade INT PRIMARY KEY AUTO_INCREMENT,
+    codigo_habilidade VARCHAR(10) NOT NULL UNIQUE,
+    habilidade VARCHAR(400) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE planejamento_quimicap (
+    id_planejamento_quimicap INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(300) NOT NULL UNIQUE,
+    objetivos VARCHAR(300) NOT NULL UNIQUE,
+    area_conhecimento VARCHAR(300) NOT NULL UNIQUE,
+    competencias_gerais VARCHAR(400) NOT NULL UNIQUE,
+    competencias_especificas VARCHAR(400) NOT NULL UNIQUE,
+    habilidades VARCHAR(400) NOT NULL UNIQUE,
+    objetos VARCHAR(400) NOT NULL UNIQUE,
+    descricao VARCHAR(400) NOT NULL UNIQUE,
+    recursos VARCHAR(255) NOT NULL UNIQUE,
+    avaliacao VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (competencias_gerais) REFERENCES naturezacg (competencia_geral) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (competencias_especificas) REFERENCES naturezace (competencia_especifica) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (habilidades) REFERENCES naturezahab (codigo_habilidade) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE planejamento_quimicas (
+    id_planejamento_quimicas INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(300) NOT NULL UNIQUE,
+    objetivos VARCHAR(300) NOT NULL UNIQUE,
+    area_conhecimento VARCHAR(300) NOT NULL UNIQUE,
+    competencias_gerais VARCHAR(400) NOT NULL UNIQUE,
+    competencias_especificas VARCHAR(400) NOT NULL UNIQUE,
+    habilidades VARCHAR(400) NOT NULL UNIQUE,
+    objetos VARCHAR(400) NOT NULL UNIQUE,
+    descricao VARCHAR(400) NOT NULL UNIQUE,
+    recursos VARCHAR(255) NOT NULL UNIQUE,
+    avaliacao VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (competencias_gerais) REFERENCES naturezacg (competencia_geral) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (competencias_especificas) REFERENCES naturezace (competencia_especifica) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (habilidades) REFERENCES naturezahab (codigo_habilidade) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE planejamento_quimicat (
+    id_planejamento_quimicat INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(300) NOT NULL UNIQUE,
+    objetivos VARCHAR(300) NOT NULL UNIQUE,
+    area_conhecimento VARCHAR(300) NOT NULL UNIQUE,
+    competencias_gerais VARCHAR(400) NOT NULL UNIQUE,
+    competencias_especificas VARCHAR(400) NOT NULL UNIQUE,
+    habilidades VARCHAR(400) NOT NULL UNIQUE,
+    objetos VARCHAR(400) NOT NULL UNIQUE,
+    descricao VARCHAR(400) NOT NULL UNIQUE,
+    recursos VARCHAR(255) NOT NULL UNIQUE,
+    avaliacao VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (competencias_gerais) REFERENCES naturezacg (competencia_geral) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (competencias_especificas) REFERENCES naturezace (competencia_especifica) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (habilidades) REFERENCES naturezahab (codigo_habilidade) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+CREATE TABLE planejamento_fisicap (
+    id_planejamento_fisicap INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(300) NOT NULL UNIQUE,
+    objetivos VARCHAR(300) NOT NULL UNIQUE,
+    area_conhecimento VARCHAR(300) NOT NULL UNIQUE,
+    competencias_gerais VARCHAR(400) NOT NULL UNIQUE,
+    competencias_especificas VARCHAR(400) NOT NULL UNIQUE,
+    habilidades VARCHAR(400) NOT NULL UNIQUE,
+    objetos VARCHAR(400) NOT NULL UNIQUE,
+    descricao VARCHAR(400) NOT NULL UNIQUE,
+    recursos VARCHAR(255) NOT NULL UNIQUE,
+    avaliacao VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (competencias_gerais) REFERENCES naturezacg (competencia_geral) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (competencias_especificas) REFERENCES naturezace (competencia_especifica) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (habilidades) REFERENCES naturezahab (codigo_habilidade) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE planejamento_fisicas (
+    id_planejamento_fisicas INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(300) NOT NULL UNIQUE,
+    objetivos VARCHAR(300) NOT NULL UNIQUE,
+    area_conhecimento VARCHAR(300) NOT NULL UNIQUE,
+    competencias_gerais VARCHAR(400) NOT NULL UNIQUE,
+    competencias_especificas VARCHAR(400) NOT NULL UNIQUE,
+    habilidades VARCHAR(400) NOT NULL UNIQUE,
+    objetos VARCHAR(400) NOT NULL UNIQUE,
+    descricao VARCHAR(400) NOT NULL UNIQUE,
+    recursos VARCHAR(255) NOT NULL UNIQUE,
+    avaliacao VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (competencias_gerais) REFERENCES naturezacg (competencia_geral) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (competencias_especificas) REFERENCES naturezace (competencia_especifica) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (habilidades) REFERENCES naturezahab (codigo_habilidade) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE planejamento_fisicat (
+    id_planejamento_fisicat INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(300) NOT NULL UNIQUE,
+    objetivos VARCHAR(300) NOT NULL UNIQUE,
+    area_conhecimento VARCHAR(300) NOT NULL UNIQUE,
+    competencias_gerais VARCHAR(400) NOT NULL UNIQUE,
+    competencias_especificas VARCHAR(400) NOT NULL UNIQUE,
+    habilidades VARCHAR(400) NOT NULL UNIQUE,
+    objetos VARCHAR(400) NOT NULL UNIQUE,
+    descricao VARCHAR(400) NOT NULL UNIQUE,
+    recursos VARCHAR(255) NOT NULL UNIQUE,
+    avaliacao VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (competencias_gerais) REFERENCES naturezacg (competencia_geral) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (competencias_especificas) REFERENCES naturezace (competencia_especifica) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (habilidades) REFERENCES naturezahab (codigo_habilidade) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+CREATE TABLE planejamento_biologiap (
+    id_planejamento_biologiap INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(300) NOT NULL UNIQUE,
+    objetivos VARCHAR(300) NOT NULL UNIQUE,
+    area_conhecimento VARCHAR(300) NOT NULL UNIQUE,
+    competencias_gerais VARCHAR(400) NOT NULL UNIQUE,
+    competencias_especificas VARCHAR(400) NOT NULL UNIQUE,
+    habilidades VARCHAR(400) NOT NULL UNIQUE,
+    objetos VARCHAR(400) NOT NULL UNIQUE,
+    descricao VARCHAR(400) NOT NULL UNIQUE,
+    recursos VARCHAR(255) NOT NULL UNIQUE,
+    avaliacao VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (competencias_gerais) REFERENCES naturezacg (competencia_geral) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (competencias_especificas) REFERENCES naturezace (competencia_especifica) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (habilidades) REFERENCES naturezahab (codigo_habilidade) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE planejamento_biologias (
+    id_planejamento_biologias INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(300) NOT NULL UNIQUE,
+    objetivos VARCHAR(300) NOT NULL UNIQUE,
+    area_conhecimento VARCHAR(300) NOT NULL UNIQUE,
+    competencias_gerais VARCHAR(400) NOT NULL UNIQUE,
+    competencias_especificas VARCHAR(400) NOT NULL UNIQUE,
+    habilidades VARCHAR(400) NOT NULL UNIQUE,
+    objetos VARCHAR(400) NOT NULL UNIQUE,
+    descricao VARCHAR(400) NOT NULL UNIQUE,
+    recursos VARCHAR(255) NOT NULL UNIQUE,
+    avaliacao VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (competencias_gerais) REFERENCES naturezacg (competencia_geral) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (competencias_especificas) REFERENCES naturezace (competencia_especifica) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (habilidades) REFERENCES naturezahab (codigo_habilidade) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE planejamento_biologiat (
+    id_planejamento_biologiat INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(300) NOT NULL UNIQUE,
+    objetivos VARCHAR(300) NOT NULL UNIQUE,
+    area_conhecimento VARCHAR(300) NOT NULL UNIQUE,
+    competencias_gerais VARCHAR(400) NOT NULL UNIQUE,
+    competencias_especificas VARCHAR(400) NOT NULL UNIQUE,
+    habilidades VARCHAR(400) NOT NULL UNIQUE,
+    objetos VARCHAR(400) NOT NULL UNIQUE,
+    descricao VARCHAR(400) NOT NULL UNIQUE,
+    recursos VARCHAR(255) NOT NULL UNIQUE,
+    avaliacao VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (competencias_gerais) REFERENCES naturezacg (competencia_geral) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (competencias_especificas) REFERENCES naturezace (competencia_especifica) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (habilidades) REFERENCES naturezahab (codigo_habilidade) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
